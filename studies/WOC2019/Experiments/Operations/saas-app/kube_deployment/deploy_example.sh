8ae9f1c73c3430065ba222a4f70a0662fd4cba9c
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2020-02-21 17:55:26 +0100

    saas-app

---
+++ studies/WOC2019/Experiments/Operations/saas-app/kube_deployment/deploy_example.sh
@@ -0,0 +1,15 @@
+for sla in `ls slas` 
+  do 
+   namespace=`sed -e 's#.*_\(\)#\1#' <<< ${sla}`
+   echo SLA configuration ${namespace}
+   echo ==============================
+
+   kubectl create -f slas/${sla}/namespace.yaml
+   kubectl create -f resourcequota/compute-resources.yaml --namespace=${namespace}
+   kubectl create -f slas/${sla}/limits.yaml --namespace=${namespace}
+   kubectl create -f deploy_saas_kube.yaml --namespace=${namespace}
+   kubectl create -f expose.yaml --namespace=${namespace}
+   kubectl get pods --namespace=${namespace}
+   kubectl describe service saas --namespace=${namespace}
+  done
+
