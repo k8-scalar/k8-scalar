94cea2467ab840b519e2df809b21908687b3fa76
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2020-11-20 16:49:51 +0100

    update

--- studies/WOC2019/Experiments/Operations/saas-app/kube_deployment/delete_deployment.sh
+++ studies/WOC2019/Experiments/Operations/saas-app/kube_deployment/delete_deployment.sh
@@ -4,11 +4,10 @@ for sla in `ls slas`
    namespace=`sed -e 's#.*_\(\)#\1#' <<< ${sla}`
    echo SLA configuration ${namespace}
    echo ==============================
-   kubectl delete -f slas/${sla}/namespace.yaml
    kubectl delete -f resourcequota/compute-resources.yaml --namespace=${namespace}
    kubectl delete -f slas/${sla}/limits.yaml --namespace=${namespace}
    kubectl delete -f deploy_saas_kube.yaml --namespace=${namespace}
    kubectl delete -f expose.yaml --namespace=${namespace}
-
+   kubectl delete -f slas/${sla}/namespace.yaml
   done
 
